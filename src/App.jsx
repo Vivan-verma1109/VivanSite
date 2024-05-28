63a28e9cdfe785ef2af49f2b00ebe75a1ef1e323
Vivan-verma1109 <vivanv732@gmail.com>
2024-05-28 04:26:17 -0700

    nun

--- src/App.jsx
+++ src/App.jsx
@@ -6,26 +6,26 @@ import Projects from "./components/Projects/projects";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
-
 const App = () => {
- return (
-   <>
-     <div className="background"></div>
-     <div className="pageMain">
-       <br />
-       <Navigation />
-       <Routes>
-         <Route path="/" element={<Ghar />} />
-         <Route path="/about" element={<About />} />
-         <Route path="/projects" element={<Projects />} />
-       </Routes>
-     </div>
-   </>
- );
-}
 
+  return (
+    <>
+      <div className="background"></div>
+      <div className="pageMain">
+        <br />
+
+        <img src = "https://assetsio.gnwcdn.com/elden-ring-all-site-of-grace-locations-8042-1648646338309.jpg?width=1600&height=900&fit=crop&quality=100&format=png&enable=upscale&auto=webp" className = "navGrace"/>
+
+
+        <Navigation />
+        <Routes>
+          <Route path="/" element={<Ghar />} />
+          <Route path="/about" element={<About />} />
+          <Route path="/projects" element={<Projects />} />
+        </Routes>
+      </div>
+    </>
+  );
+};
 
 export default App;
-
-
-
