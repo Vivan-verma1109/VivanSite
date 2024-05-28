1a9d3acc5e14bae69cfc71e31193092f75209dff
Vivan Verma <vivanverma@Vivans-MBP.attlocal.net>
2024-05-28 04:00:11 -0700

    starting to add grace nav bar

--- src/App.jsx
+++ src/App.jsx
@@ -4,23 +4,28 @@ import About from "./components/Information/About";
 import Navigation from "./components/Navbar/Navigation";
 import Projects from "./components/Projects/projects";
 import { Routes, Route } from "react-router-dom";
-import "./App.css"; 
+import "./App.css";
+
 
 const App = () => {
-  return (
-    <>
-      <div className="background"></div>
-      <div className="pageMain"> 
-        <br />
-        <Navigation />
-        <Routes>
-          <Route path="/" element={<Ghar />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-        </Routes>
-      </div>
-    </>
-  );
+ return (
+   <>
+     <div className="background"></div>
+     <div className="pageMain">
+       <br />
+       <Navigation />
+       <Routes>
+         <Route path="/" element={<Ghar />} />
+         <Route path="/about" element={<About />} />
+         <Route path="/projects" element={<Projects />} />
+       </Routes>
+     </div>
+   </>
+ );
 }
 
+
 export default App;
+
+
+
