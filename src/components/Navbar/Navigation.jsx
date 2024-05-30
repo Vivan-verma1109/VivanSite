6d10f358818ee035c1723a429dc6250bbbbf59bd
Vivan-verma1109 <vivanv732@gmail.com>
2024-05-30 15:17:11 -0700

    adding navbar icons

--- src/components/Navbar/Navigation.jsx
+++ src/components/Navbar/Navigation.jsx
@@ -2,13 +2,24 @@ import React from "react";
 import { NavLink, useLocation } from "react-router-dom";
 import "./nav.css";
 
-const Navigation = () => {
+const Navigation = ({ showNavigation }) => {
   const location = useLocation();
 
   return (
     <>
-      <div class="entire">
-        <img src="https://static.thenounproject.com/png/992984-200.png" className = "aboutIcon1"/>
+      <div className="entire">
+        <img
+          src="https://static.thenounproject.com/png/992984-200.png"
+          className={showNavigation ? "aboutIcon2" : "aboutIcon1"}
+        />
+        <img
+          src="https://i.pinimg.com/originals/71/c4/14/71c41419a3a25b09742aaf3dc648bb11.png"
+          className={showNavigation ? "proj2" : "proj1"}
+        />
+        <img
+          src="https://cdn-icons-png.freepik.com/512/2/2144.png"
+          className={showNavigation ? "home2" : "home1"}
+        />
       </div>
     </>
   );
